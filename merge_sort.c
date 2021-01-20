18cd8da877ddca5fdc709a9a8f197a9e1fca9eae
b05902132 <b05902132@csie.ntu.edu.tw>
2021-01-20 14:29:44 +0800

    Merge sort first loop permutation done.

--- merge_sort.c
+++ merge_sort.c
@@ -7,7 +7,8 @@
 /*@
   axiomatic count_elem_axiom{
     logic integer count_elem{L}(int elem, int *arr, integer b, integer e) reads arr[b..e-1];
-    axiom empty : \forall int elem, int *arr, integer b, e; b >= e ==> count_elem(elem, arr, b, e) == 0;
+    axiom empty :
+        \forall int elem, int *arr, integer b, e; b >= e ==> count_elem(elem, arr, b, e) == 0;
     axiom append_eq : \forall int elem, int *arr, integer b, e;
         arr[e] == elem ==> count_elem(elem, arr, b, e+1) == count_elem(elem, arr, b, e) + 1;
     axiom append_neq : \forall int elem, int *arr, integer b, e;
@@ -17,11 +18,30 @@
   predicate permutation{L1, L2}(int *arr1, int *arr2, integer b, integer e) =
     \forall int v; count_elem{L1}(v, arr1, b, e) == count_elem{L2}(v, arr2, b, e);
 
+
   lemma perm_eq{L1, L2}: \forall int *arr1, int *arr2, integer b, integer e;
       (\forall integer i; b <= i < e ==> \at(arr1[i], L1) == \at(arr2[i], L2))
         ==> permutation{L1, L2}(arr1, arr2, b, e);
 
-  predicate sorted(int *arr, integer len) = \forall integer i; 0 <= i < len-1  ==> arr[i] <= arr[i+1];
+  predicate unchanged{L1, L2}(int *arr, integer b, integer e) =
+    \forall integer i; b <= i < e ==> \at(arr[i], L1) == \at(arr[i], L2);
+
+  predicate count_unchanged{L1, L2}(int *arr, integer b, integer e) =
+    \forall int elem; count_elem{L1}(elem, arr, b, e) == count_elem{L2}(elem, arr, b, e);
+
+  lemma unchanged_same_count{L1, L2} :
+    \forall int *arr, integer b, e;
+      unchanged{L1, L2}(arr, b, e) ==> count_unchanged{L1, L2}(arr, b, e);
+
+
+  predicate sorted{L}(int *arr, integer len) =
+    \forall integer i, j; 0 <= i <= j < len  ==> arr[i] <= arr[j];
+
+  lemma unchange_sorted{L1, L2}:
+    \forall int *arr, integer len;
+      unchanged{L1, L2}(arr, 0, len) && sorted{L1}(arr, len)
+      ==> sorted{L2}(arr, len);
+
 
   lemma sorted_append: \forall int *arr, integer len;
     sorted(arr, len) && arr[len] >= arr[len - 1] ==> sorted(arr, len + 1);
@@ -49,11 +69,8 @@ static unsigned int * const buf_end = BUFFER + BUFFER_SIZE;
     \forall int *a1, *a2, *out, integer size1, size2;
         count_combine(a1, size1, a2, size2, out) && a2[size2] == out[size1 + size2]
             ==> count_combine(a1, size1, a2, size2+1, out);
-
-
 */
 
-//@ logic int min(int a, int b) = a <= b ? a : b;
 
 /*@
   requires \valid_read(arr1 + (0..len1-1));
@@ -61,8 +78,8 @@ static unsigned int * const buf_end = BUFFER + BUFFER_SIZE;
   requires \separated(out + (0 .. len1 + len2 - 1), arr1 + (0 .. len1 - 1));
   requires \separated(out + (0 .. len1 + len2 - 1), arr2 + (0 .. len2 - 1));
   requires len1 + len2 <= SIZE_MAX;
-  requires sorted(arr1, len1);
-  requires sorted(arr2, len2);
+  requires arr1_sorted: sorted(arr1, len1);
+  requires arr2_sorted: sorted(arr2, len2);
   requires \valid(out + (0..len1 + len2 - 1));
   assigns out[0..len1 + len2 - 1];
   ensures sorted(out, len1 + len2);
@@ -70,33 +87,56 @@ static unsigned int * const buf_end = BUFFER + BUFFER_SIZE;
 */
 void merge(int *arr1, size_t len1, int *arr2, size_t len2, int *out)
 {
-    size_t i1 = 0;
-    size_t i2 = 0;
+    size_t i_1 = 0;
+    size_t i_2 = 0;
+    size_t i_out = 0; // this variable aids theorem prover.
     /*@
-      loop invariant 0 <= i1 <= len1;
-      loop invariant 0 <= i2 <= len2;
-      loop invariant i1 <= len1 ==>
-        \forall integer i; 0 <= i < i1 + i2 ==> out[i] <= arr1[i1];
-      loop invariant i2 <= len2 ==>
-        \forall integer i; 0 <= i < i1 + i2 ==> out[i] <= arr2[i2];
-      loop invariant sorted(out, i1 + i2);
-      loop invariant count_combine(arr1, i1, arr2, i2, out);
-      loop assigns out[0..len1 + len2 - 1], i1, i2;
-      loop variant len1 + len2 - i1 - i2;
+      loop invariant 0 <= i_1 <= len1;
+      loop invariant 0 <= i_2 <= len2;
+      loop invariant i_out == i_1 + i_2;
+      loop invariant out_bounded_arr1:
+        i_1 < len1 ==> \forall integer i; 0 <= i < i_1 + i_2 ==> out[i] <= arr1[i_1];
+      loop invariant out_bounded_arr2:
+        i_2 < len2 ==> \forall integer i; 0 <= i < i_1 + i_2 ==> out[i] <= arr2[i_2];
+      loop invariant sorted(out, i_out);
+      loop invariant count_combine(arr1, i_1, arr2, i_2, out);
+      loop assigns out[0..i_out - 1], i_1, i_2, i_out;
+      loop variant len1 + len2 - i_out;
     */
-    while (i1 < len1 && i2 < len2) {
-        if (arr1[i1] > arr2[i2]) {
-            out[i1 + i2] = arr2[i2];
-            i2 += 1;
-            //@ assert i1 <len1 ==> out[i1 + i2 - 1] <= arr1[i1];
-            //@ assert i2 <len2 ==> out[i1 + i2 - 1] <= arr2[i2];
+    while (i_1 < len1 && i_2 < len2) {
+        if (arr1[i_1] > arr2[i_2]) {
+            out[i_out] = arr2[i_2];
+            /* The auto prover are not smart enough to prove loop1_append_i2;
+             * they require some aid to prove that the counts are not affected by this assignment,
+             * hence the assertions about count_unchanged.
+             * However, they cannot be autoproved without the first 3 assertions...
+             */
+            //@ assert unchanged{LoopCurrent, Here}(arr1, 0, i_1);
+            //@ assert unchanged{LoopCurrent, Here}(arr2, 0, i_2);
+            //@ assert unchanged{LoopCurrent, Here}(out, 0, i_1 + i_2);
+            //@ assert count_unchanged{LoopCurrent, Here}(arr1, 0, i_1);
+            //@ assert count_unchanged{LoopCurrent, Here}(arr2, 0, i_2);
+            //@ assert count_unchanged{LoopCurrent, Here}(out, 0, i_1 + i_2);
+            //@ assert loop1_append_i2: count_combine(arr1, i_1, arr2, i_2+1, out);
+            i_2 += 1;
+            i_out += 1;
+            //@ assert bound_aid_arr2: i_2 < len2 ==> out[i_1 + i_2 - 1] <= arr2[i_2];
+            //@ assert sorted(out, i_1 + i_2 - 1);
         } else {
-            out[i1 + i2] = arr1[i1];
-            i1 += 1;
+            out[i_out] = arr1[i_1];
+            //@ assert unchanged{LoopCurrent, Here}(arr1, 0, i_1);
+            //@ assert unchanged{LoopCurrent, Here}(arr2, 0, i_2);
+            //@ assert unchanged{LoopCurrent, Here}(out, 0, i_1 + i_2);
+            //@ assert count_unchanged{LoopCurrent, Here}(arr1, 0, i_1);
+            //@ assert count_unchanged{LoopCurrent, Here}(arr2, 0, i_2);
+            //@ assert count_unchanged{LoopCurrent, Here}(out, 0, i_1 + i_2);
+            //@ assert loop1_append_i1: count_combine(arr1, i_1+1, arr2, i_2, out);
+            i_1 += 1;
+            i_out += 1;
+            //@ assert bound_aid_arr1: i_1 < len1 ==> out[i_1 + i_2 - 1] <= arr1[i_1];
+            //@ assert sorted(out, i_1 + i_2 - 1);
         }
-        //@ assert out[i1 + i2 - 1] == \at(min(arr1[i1], arr2[i2]), LoopCurrent);
-        //@ assert i1 <len1 && i2 <len2 ==> out[i1 + i2 - 1] <= min(arr1[i1], arr2[i2]);
-        //@ assert i1 <len1 && i2 <len2 ==> \at(min(arr1[i1], arr2[i2]), LoopCurrent) <= min(arr1[i1], arr2[i2]);
+        //@ assert i_1 + i_2 >= 2 ==> out[i_1 + i_2 - 2] <= out[i_1 + i_2 - 1];
     }
 }
 
