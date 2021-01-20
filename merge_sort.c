c8e0fe8265ff3088d541c7391ba645d44a61a724
b05902132 <b05902132@csie.ntu.edu.tw>
2021-01-20 15:10:10 +0800

    merge_sort sketch

--- merge_sort.c
+++ merge_sort.c
@@ -3,6 +3,10 @@
 #include <stddef.h>
 #define LEN 10000
 #define BUF_SIZE LEN * 3
+
+// Two buffers are needed to prevent aliasing problems.
+// See static_buffer1.c and its proof script for IMPOSSIBLE for details.
+
 static int buf1[BUF_SIZE];
 size_t buf1_used;
 static int buf2[BUF_SIZE];
@@ -54,11 +58,6 @@ size_t buf2_used;
     sorted(arr, len) ==> \forall integer i, j; 0 <= i <= j < len ==> arr[i] <= arr[j];
 */
 
-static unsigned int BUFFER[BUFFER_SIZE];
-
-static unsigned int *buf = BUFFER;
-
-static unsigned int * const buf_end = BUFFER + BUFFER_SIZE;
 
 
 /*@
@@ -160,6 +159,26 @@ void merge(int *arr1, size_t len1, int *arr2, size_t len2, int *out)
 void merge2(int *arr1, size_t len1, int *arr2, size_t len2, int *out);
 
 /*@
+  requires \valid(out + (0 .. len-1));
+  requires \valid_read(in + (0 .. len-1));
+  requires \separated(in + (0 .. len-1), out + (0 .. len-1));
+  assigns out[0..len-1];
+  ensures \forall integer i; 0 <= i < len ==> in[i] == out[i];
+*/
+void arrcpy(int *out, const int *in, size_t len)
+{
+    /*@
+      loop invariant 0 <= i <= len;
+      loop invariant \forall integer j; 0 <= j < i ==> out[j] == in[j];
+      loop assigns i, out[0..len-1];
+      loop variant len - i;
+    */
+    for(size_t i = 0; i < len; i++){
+        out[i] = in[i];
+    }
+}
+
+/*@
   requires \valid(arr + (0 .. len - 1));
   requires \separated(arr + (0.. len - 1), (buf1 + (buf1_used .. buf1_used + len - 1)));
   requires \separated(arr + (0.. len - 1), (buf2 + (buf2_used .. buf2_used + len * 2- 1)));
@@ -179,6 +198,13 @@ void merge_sort(int *arr, size_t len)
         return;
     }
     if (len == 2) {
+        if (arr[0] > arr[1]) {
+            int tmp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = tmp;
+        }
+        //@ assert permutation{Pre, Here}(arr, arr, 0, len);
+        //@ assert sorted(arr, len);
     }
     int *const local_buf1 = buf1 + buf1_used;
     buf1_used += len/2;
@@ -187,14 +213,13 @@ void merge_sort(int *arr, size_t len)
     buf2_used += (len - len/2);
     arrcpy(local_buf2, arr + len/2, len - len/2);
 
-    //@ assert \forall integer i; 0 <= i < len/2 ==> arr[i] == local_buf1[i];
-    //@ assert \forall integer i; 0 <= i < len - len/2 ==> arr[len/2 + i] == local_buf2[i];
+    //@ assert sorted(local_buf1, len/2);
+    //@ assert sorted(local_buf2, len - len/2);
 
-    recur(local_buf1, len/2);
-    recur(local_buf2, len - len/2);
+    merge_sort(local_buf1, len/2);
+    merge_sort(local_buf2, len - len/2);
 
-    arrcpy(arr, local_buf1, len/2);
-    arrcpy(arr + len/2, local_buf2, len - len/2);
+    merge2(local_buf1, len/2, local_buf2, len - len/2, arr);
 
     buf1_used -= len/2;
     buf2_used -= len - len/2;
