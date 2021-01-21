2d5f0f31ebdb01b2824dde3de869a6c1b24f888f
b05902132 <b05902132@csie.ntu.edu.tw>
2021-01-21 20:32:27 +0800

    ...

--- merge.c
+++ merge.c
@@ -22,9 +22,9 @@ void merge(const int *arr1, size_t len1, const int *arr2, size_t len2, int *out)
       loop invariant 0 <= i_2 <= len2;
       loop invariant i_out == i_1 + i_2;
       loop invariant out_bounded_arr1:
-        i_1 < len1 ==> \forall integer i; 0 <= i < i_1 + i_2 ==> out[i] <= arr1[i_1];
+        i_1 < len1 && i_out > 0 ==> out[i_out - 1] <= arr1[i_1];
       loop invariant out_bounded_arr2:
-        i_2 < len2 ==> \forall integer i; 0 <= i < i_1 + i_2 ==> out[i] <= arr2[i_2];
+        i_2 < len2 && i_out > 0 ==> out[i_out - 1] <= arr2[i_2];
       loop invariant sorted(out, i_out);
       loop invariant count_combine(arr1, i_1, arr2, i_2, out);
       loop assigns out[0..i_out - 1], i_1, i_2, i_out;
@@ -42,14 +42,10 @@ void merge(const int *arr1, size_t len1, const int *arr2, size_t len2, int *out)
             //@ assert same_array{LoopCurrent, Here}(arr1, arr1, i_1);
             //@ assert same_array{LoopCurrent, Here}(arr2, arr2, i_2);
             //@ assert same_array{LoopCurrent, Here}(out, out, i_1 + i_2);
-            //@ assert permutation{LoopCurrent, Here}(arr1, arr1, i_1);
-            //@ assert permutation{LoopCurrent, Here}(arr2, arr2, i_2);
-            //@ assert permutation{LoopCurrent, Here}(out, out, i_out);
             //@ assert loop1_append_i2: count_combine(arr1, i_1, arr2, i_2+1, out);
 
             i_2 += 1;
             i_out += 1;
-            //@ assert bound_aid_arr2: i_2 < len2 ==> out[i_1 + i_2 - 1] <= arr2[i_2];
         } else {
             out[i_out] = arr1[i_1];
             //@ assert i_out-1 > 0 ==> out[i_out - 1] <= out[i_out];
@@ -58,16 +54,68 @@ void merge(const int *arr1, size_t len1, const int *arr2, size_t len2, int *out)
             //@ assert same_array{LoopCurrent, Here}(arr1, arr1, i_1);
             //@ assert same_array{LoopCurrent, Here}(arr2, arr2, i_2);
             //@ assert same_array{LoopCurrent, Here}(out, out, i_1 + i_2);
-            //@ assert permutation{LoopCurrent, Here}(arr1, arr1, i_1);
-            //@ assert permutation{LoopCurrent, Here}(arr2, arr2, i_2);
-            //@ assert permutation{LoopCurrent, Here}(out, out, i_out);
 
             //@ assert loop1_append_i1: count_combine(arr1, i_1+1, arr2, i_2, out);
             i_1 += 1;
             i_out += 1;
-            //@ assert bound_aid_arr1: i_1 < len1 ==> out[i_1 + i_2 - 1] <= arr1[i_1];
-            //@ assert sorted(out, i_out);
         }
-        //@ assert i_1 + i_2 >= 2 ==> out[i_1 + i_2 - 2] <= out[i_1 + i_2 - 1];
+    }
+
+    if (i_1 < len1) {
+        /*@
+          loop invariant \at(i_1, LoopEntry) <= i_1 <= len1;
+          loop invariant i_out == i_1 + i_2;
+          loop invariant out_bounded_arr1_again:
+                i_1 < len1 && i_out > 0 ==> out[i_out - 1] <= arr1[i_1];
+          loop invariant sorted(out, i_out);
+          loop invariant count_combine(arr1, i_1, arr2, i_2, out);
+          loop assigns i_1, i_out, out[\at(i_out, LoopEntry).. len1 + len2 - 1];
+          loop variant len1 - i_1;
+        */
+        while (i_1 < len1) {
+            out[i_out] = arr1[i_1];
+            //@ assert i_out > 0 ==> out[i_out - 1] <= out[i_out];
+            //@ assert i_1 + 1 < len1 ==> out[i_out] <= arr1[i_1+1];
+            //@ assert sorted(out, i_out + 1);
+
+            //@ assert same_array{LoopCurrent, Here}(arr1, arr1, i_1);
+            //@ assert same_array{LoopCurrent, Here}(arr2, arr2, i_2);
+            //@ assert same_array{LoopCurrent, Here}(out, out, i_out);
+
+            //@ assert loop1_append_i1_loop2: count_combine(arr1, i_1+1, arr2, i_2, out);
+            i_1 += 1;
+            i_out += 1;
+            //@ assert bound_aid_arr1_loop2: i_1 < len1 ==> out[i_out - 1] <= arr1[i_1]; // took me 26 seconds....
+        }
+    } else if (i_2 < len2) {
+        /*@
+          loop invariant \at(i_2, LoopEntry) <= i_2 <= len2;
+          loop invariant i_out == i_1 + i_2;
+          loop invariant out_bounded_arr2_again:
+                i_2 < len2 && i_out > 0 ==> out[i_out - 1] <= arr2[i_2];
+          loop invariant sorted(out, i_out);
+          loop invariant count_combine(arr1, i_1, arr2, i_2, out);
+          loop assigns i_2, i_out, out[\at(i_out, LoopEntry).. len1 + len2 - 1];
+          loop variant len2 - i_2;
+        */
+        while (i_2 < len2) {
+            out[i_out] = arr2[i_2];
+            //@ assert i_out > 0 ==> out[i_out - 1] <= out[i_out];
+            //@ assert i_2 + 1 < len2 ==> out[i_out] <= arr2[i_2+1];
+            //@ assert sorted(out, i_out + 1);
+
+            //@ assert same_array{LoopCurrent, Here}(arr1, arr1, i_1);
+            //@ assert same_array{LoopCurrent, Here}(arr2, arr2, i_2);
+            //@ assert same_array{LoopCurrent, Here}(out, out, i_out);
+
+            //@ assert loop1_append_i2_loop2: count_combine(arr1, i_1, arr2, i_2 + 1, out);
+            i_2 += 1;
+            i_out += 1;
+            //@ assert bound_aid_arr2_loop2: i_2 < len2 ==> out[i_out - 1] <= arr2[i_2];
+        }
+    } else {
+        //@ assert sorted(out, len1 + len2);
+        //@ assert count_combine(arr1, len1, arr2, len2, out);
+
     }
 }
